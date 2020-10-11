729c69e369702e4b68812ee1abd3df97890d0082
Filip Mularski <mularskif@gmail.com>
2020-10-11 20:16:36 +0200

    filter todos

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filter = document.querySelector('.filter-todo');
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', completeRemove);
+filter.addEventListener('change', filterTodos);
 
 function addTodo(event){
     event.preventDefault();
@@ -45,4 +47,32 @@ function completeRemove(event){
         parent.classList.toggle('completed');
         clickedBtn.classList.toggle('completed');
     }
+}
+
+function filterTodos(event) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+    todos.forEach((todo) => {
+        switch(filter.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            
+            case "completed":
+                if (todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            
+            case "uncompleted":
+                if (todo.classList.contains('completed')){
+                    todo.style.display = 'none';
+                } else {
+                    todo.style.display = 'flex';
+                }
+                break;
+        }
+    })
 }
